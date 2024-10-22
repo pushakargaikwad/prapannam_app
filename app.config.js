0fe5aeeaf55bfbca7f0e5b8366e4e267d1a33236
pushakar <pushakargaikwad@gmail.com>
2024-10-22 12:19:09 +0530

    [ADD] App variants and EAS Build

--- app.config.js
+++ app.config.js
@@ -1,13 +1,44 @@
+const IS_DEV = process.env.APP_VARIANT === 'development';
+const IS_PREVIEW = process.env.APP_VARIANT === 'preview';
+
+const getUniqueIdentifier = () => {
+  if (IS_DEV) {
+    console.log('Running in DEV mode');
+    return 'com.prapannam.dev';
+  }
+
+  if (IS_PREVIEW) {
+    console.log('Running in PREVIEW mode');
+    return 'com.prapannam.preview';
+  }
+
+  return 'com.prapannam';
+};
+
+const getAppName = () => {
+  if (IS_DEV) {
+    return 'Prapannam (Dev)';
+  }
+
+  if (IS_PREVIEW) {
+    return 'Prapannam (Preview)';
+  }
+
+  return 'Prapannam';
+};
+
+
 module.exports = ({ config }) => {
   console.log(config.name); // prints 'My App'
   return {
     ...config,
     "expo": {
       ...config.expo,
+      name: getAppName(),
       slug: "prapannam",
       "android": {
         ...(config.android || {}),
-        "package": "com.prapannam"
+        "package": getUniqueIdentifier(),
       },
       "web": {
         ...(config.web || {}),
@@ -15,6 +46,8 @@ module.exports = ({ config }) => {
       "output": "static",
       "favicon": "./assets/images/favicon.png",
       "scheme": "com.prapannam",
+      
+    
     },
     "scheme": "com.prapannam",
     "plugins": [
@@ -30,7 +63,14 @@ module.exports = ({ config }) => {
       [
         "expo-secure-store"
       ]
-    ]
+    ],
+    "extra": {
+        "eas": {
+          "projectId": process.env.EXPO_PUBLIC_PROJECT_ID
+        }
+      },
+    "owner": process.env.EXPO_PUBLIC_OWNER
+
   }
   };
 };
