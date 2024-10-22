6f1d0f642c20bf972405081757612581965722cc
pushakar <pushakargaikwad@gmail.com>
2024-10-22 12:16:30 +0530

    [ADD] restrict routes to non-authenticated

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { Link, Tabs } from 'expo-router';
+import { Link, Redirect, Tabs } from 'expo-router';
 import { Pressable } from 'react-native';
 
 import Colors from '@/constants/Colors';
 import { useColorScheme } from '@/components/useColorScheme';
 import { useClientOnlyValue } from '@/components/useClientOnlyValue';
+import { AuthContext } from '../FrappeAuthProvider';
 
 // You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
 function TabBarIcon(props: {
@@ -17,7 +18,10 @@ function TabBarIcon(props: {
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
-
+  const {isAuthenticated, promptAsync,logout} = useContext(AuthContext)
+  if(!isAuthenticated){
+    return <Redirect href="/sign-in" />;
+  }
   return (
     <Tabs
       screenOptions={{
